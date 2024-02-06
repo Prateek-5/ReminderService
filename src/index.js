7d30abd3541f11908a5083cd0adb9998b25eff7c
Prateek-5 <prateek.ku11@gmail.com>
2024-02-06 19:11:24 +0530

    setup of smtp using nodemailer done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express=require('express');
 
 const bodyParser= require('body-parser');
 const {PORT}=require('./config/serverConfig');
+const {sendBasicEmail}=require('./services/email-service');
 
 const setupAndStartServer =() =>{
     const app=express();
@@ -10,6 +11,14 @@ const setupAndStartServer =() =>{
 
     app.listen(PORT,()=>{
         console.log(`Server is running on port ${PORT}`);
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'notificationservice85@gmail.com',
+        //     'This is a testing email',
+        //     'Hey, how are you , I hope you like the support',
+
+        // );
+        
     })
 }
 
---
+++ src/services/email-service.js
@@ -0,0 +1,21 @@
+const sender=require('../config/email-Config');
+
+const sendBasicEmail=(mailFrom, mailTo, mailSubject,mailBody)=>{
+    
+    try {
+        sender.sendMail({
+            from:mailFrom,
+            to:mailTo,
+            subject:mailSubject,
+            text:mailBody
+    
+        })
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+module.exports={
+    sendBasicEmail
+}
